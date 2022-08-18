3b6472d07fe0d16e7a56f91b3b6e51308ef1342c
Smaug6739 <veauville.raphael@gmail.com>
2022-08-18 15:17:55 +0200

    Updates complexs

--- src/views/maths/algebres/nombres_complexes.vue
+++ src/views/maths/algebres/nombres_complexes.vue
@@ -3,28 +3,28 @@
 <template>
   <main>
     <h1>Nombres complexes</h1>
-    <h2>I Définitions et notations</h2>
-    <h3>A) Théorème</h3>
+    <hr>
+    <h2>I Théorème</h2>
     <ul>
       <li>On admet l'existance d'un ensemble de nombres appelé <strong>nombres complexes</strong> :
         <katex src="\mathbb{C}" />, contenant
         <katex src="\mathbb{R}" /> l'ensemble de tous les réels.
       </li>
-      <li>
-        <katex src="\mathbb{C}" /> est muni de deux opérations élémentaires, l'addition et la multiplication.
-      </li>
+
       <li>
         <katex src="\mathbb{C}" /> comporte un nombre noté i, le nombre imaginaire, tel que
         <code> <katex src="i ^2 = - 1" /></code>.
       </li>
-      <li>Tout élement z de
+    </ul>
+    <h2>II Représentation algébrique d'un nombre complexe</h2>
+    <h3>A) Définitions et notations</h3>
+    <ul>
+
+      <li> Tout élement z de
         <katex src="\mathbb{C}" /> possède une <strong>écriture unique</strong> sous la forme
         <code> <katex src=" a + ib,\:(a
         ,b) \in \mathbb{R}^2" /></code>.
       </li>
-    </ul>
-    <h3>B) Vocabulaire</h3>
-    <ul>
       <li>Dans la notation d'un nombre complexe, on trouve deux parties distinctes, la partie réelle et la partie
         imaginaire. Prennons
         <katex src="z = a +ib" /> , ici
@@ -37,11 +37,13 @@
         <katex src="z = a + ib" />.
       </li>
       <li>
-        Si la partie réelle d'un nombre complexe est nulle, on dit qu'il s'agit d'un nombre imaginaire pur.
+        Si la partie réelle d'un nombre complexe est nulle, on dit qu'il s'agit d'un nombre <strong>imaginaire
+          pur</strong>.
       </li>
     </ul>
-    <h2> II Conjugué d'un nombre complexe</h2>
-    <p>Pour tout nombre complexe
+    <h3>B) Conjugué d'un nombre complexe</h3>
+    <Info name="Définition:" color="red">
+      Pour tout nombre complexe
       <katex src="z" /> de la forme algébrique
       <katex src="z = a + ib" />, on a avec
       <katex src="a" /> et
@@ -52,9 +54,9 @@
       <katex src="\bar{z} = a - ib" />. Ainsi,
       <katex src="Re(\bar{z}) = Re(z)" /> et
       <katex src="Im(\bar{z}) = -Im(z)" />.
-      <br />
-      Exemples : <br />
-    <ul class="inline-list">
+    </Info>
+    <h4>Exemples:</h4>
+    <ul>
       <li>
         <katex src="\bar{z_1} = \overline{1-2i} = 1 + 2i" />
       </li>
@@ -68,17 +70,70 @@
         <katex src="\bar{z_4} = \overline{2i} = -2i" />
       </li>
     </ul>
-    <ul class="inline-list">
+    <h3>Module d'un nombre complexe</h3>
+    <Info name="Définition:" color="red">
+
+      Le <strong>module</strong> d'un nombre complexe
+      <katex src="z = a + ib" /> est le nombre <strong>réel positif</strong>
+      <katex src=" |z| = \sqrt{a^2 + b^2}" />.
+    </Info>
+    <h4>Exemples:</h4>
+    <ul>
       <li>
-        a
+        <katex src="|z_1| = |1-2i| = \sqrt{1^2+2^2} = \sqrt{1+4} = \sqrt{5}" />
       </li>
-      <li>b
+      <li>
+        <katex src="|z_2| = |3+i| = \sqrt{3^2+1^2} = \sqrt{10}" />
       </li>
-      <li>c
+      <li>
+        <katex src="|z_3| = |-3| = \sqrt{-3^2} = \sqrt{9} = 3" />
       </li>
-      <li>d
+      <li>
+        <katex src="|z_4| = |2i| = \sqrt{2^2} = \sqrt{4} = 2" />
       </li>
     </ul>
+    <h3>Opérations sur des nombres complexes</h3>
+    <h4>L'addition et la soustraction</h4>
+    <p>
+      <Info name="Propriété:">Les nombres complexes sont <strong>additifs</strong> et <strong>soustractifs</strong>. On
+        les additionne et on
+        les
+        soustrait de la même manière que des nombres appartenant à l'ensembles des réels
+        <katex src="\mathbb{R}" />.
+      </Info>
+    </p>
+    <p>
+      Exemple:
+      <katex src="z_1 + z_2 = (1 +2i) + (3 +i) = 1 - 2i + 3 3 i = 4-i" />
+    </p>
+    <h4>La multiplication</h4>
+    <p>
+      <Info name="Propriété:">La multiplication d'un nombre complexe par un nombre réel ou un nombre complexe suivent
+        les mêmes règles que
+        pour
+        l'ensemble des réels
+        <katex src="\mathbb{R}" /> avec
+        <katex src="i^2 = -1" />.
+      </Info>
+
+      <Warning name="Cas particulier:">
+        Lors d'une multiplication d'un nombre complexe
+        <katex src="z = a + ib" /> par son conjugué
+        <katex src="z \times \bar{z} = a^2 + b^2" />.
+      </Warning><br /><br />
+      Exemple:
+      <katex src="z_1 \times z_2 = (1 - 2i) \times (3 +i) = 3+i-6i-2i^2=3-5i+2=5-5i" />
+    </p>
+    <h4>La division de nombres complexes</h4>
+    <p>
+      <Info name="Propriété:">Le quotient de deux nombres complexes (avec un dénominateur non nul) est un nombre
+        complexe.</Info>
+    </p>
+    <p>
+      <i style="text-decoration: underline;">Méthode:</i> Pour mettre sous forme algébrique un quotient, l'idée est de
+      multiplier le numérateur et le dénominateur par le conjugué du dénominateur afin de transformer le dénominateur en
+      un nombre réel (
+      <katex src="\mathbb{R}" />).
     </p>
   </main>
 </template>
@@ -87,11 +142,15 @@
 
 import { defineComponent } from "vue";
 import katex from '../../../components/Math.vue';
+import Warning from '../../../components/common/Warning.vue';
+import Info from '../../../components/common/Info.vue';
 
 export default defineComponent({
   name: "NombresComplexes",
   components: {
-    katex
+    katex,
+    Warning,
+    Info
   },
 });
 </script>
